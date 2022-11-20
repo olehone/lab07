ebab1d9998fefda37f9337c5f618bcf90fc87ea7
olehvolodko <oleh.volodko.ri.2022@lpnu.ua>
2022-11-20 12:30:56 +0200

    lab7.1 it

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Source.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int rowCount = 2;
+			int colCount = 2;
+			int** a = new int* [rowCount];
+			for (int i = 0; i < rowCount; i++)
+				a[i] = new int[colCount];
+			a[0][0] = 1;
+			a[1][0] = 5;
+			a[0][1] = -2;
+			a[1][1] = 7;
+			Assert::AreEqual(CalcSum(a, rowCount, colCount, 0), 6);
+		}
+	};
+}
